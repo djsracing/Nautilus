adf4970dafe8c20f263e900e3175ca5bcbcfd3a6
atharva-18 <atharvapusalkar18@gmail.com>
2020-05-09 12:37:14 +0530

    Resized note-item

--- templates/assets/js/apps/notes.js
+++ templates/assets/js/apps/notes.js
@@ -7,6 +7,44 @@ var sline = {
     },
     toolbar: {
       show: false,
+    },
+    events: {
+      mounted: function(ctx, config) {
+        const highest1 = ctx.getHighestValueInSeries(0);
+        // const highest2 = ctx.getHighestValueInSeries(1);
+
+        ctx.addPointAnnotation({
+          x: new Date(ctx.w.globals.seriesX[0][ctx.w.globals.series[0].indexOf(highest1)]).getTime(),
+          y: highest1,
+          label: {
+            style: {
+              cssClass: 'd-none'
+            }
+          },
+          customSVG: {
+              SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#1b55e2" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+              cssClass: undefined,
+              offsetX: -8,
+              offsetY: 5
+          }
+        })
+
+        // ctx.addPointAnnotation({
+        //   x: new Date(ctx.w.globals.seriesX[1][ctx.w.globals.series[1].indexOf(highest2)]).getTime(),
+        //   y: highest2,
+        //   label: {
+        //     style: {
+        //       cssClass: 'd-none'
+        //     }
+        //   },
+        //   customSVG: {
+        //       SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#e7515a" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+        //       cssClass: undefined,
+        //       offsetX: -8,
+        //       offsetY: 5
+        //   }
+        // })
+      },
     }
   },
   dataLabels: {
