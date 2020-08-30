7f315e7ca6e0463244470d418f9103968d0244d2
atharva-18 <atharvapusalkar18@gmail.com>
2020-08-30 20:45:30 +0530

    Added GG plot options

--- templates/assets/js/dashboard/dash_2.js
+++ templates/assets/js/dashboard/dash_2.js
@@ -5,66 +5,12 @@ try {
   }
 
 /*
-    ==============================
-    |    @Options Charts Script   |
-    ==============================
+    =============================
+        Motor RPM | Options
+    =============================
 */
 
-/*
-    =============================
-        Daily Sales | Options
-    =============================
-*/
 var d_2options1 = {
-  chart: {
-    id: 'sparkline1',
-    type: 'area',
-    height: 160,
-    sparkline: {
-      enabled: true
-    },
-  },
-  stroke: {
-      curve: 'smooth',
-      width: 2,
-  },
-  fill: {
-    opacity: 1,
-  },
-  series: [{
-    name: 'RPM',
-    data: [28, 50, 36, 60, 38, 52, 38 ]
-  }],
-  labels: ['1', '2', '3', '4', '5', '6', '7'],
-  yaxis: {
-    min: 0
-  },
-  colors: ['#009688'],
-  tooltip: {
-    enabled: true,
-    x: {
-      show: true,
-    }
-  },
-  fill: {
-      type:"gradient",
-      gradient: {
-          type: "vertical",
-          shadeIntensity: 1,
-          inverseColors: !1,
-          opacityFrom: .40,
-          opacityTo: .05,
-          stops: [45, 100]
-      }
-  },
-}
-
-/*
-    =============================
-        Total Orders | Options
-    =============================
-*/
-var d_2options2 = {
   chart: {
     id: 'sparkline1',
     group: 'sparklines',
@@ -74,6 +20,7 @@ var d_2options2 = {
       enabled: true
     },
   },
+  colors: ['#009688'],
   stroke: {
       curve: 'smooth',
       width: 2
@@ -82,10 +29,9 @@ var d_2options2 = {
     opacity: 1,
   },
   series: [{
-    name: 'Charge %',
-    data: [28, 40, 36, 52, 38, 60, 38, 52, 36, 40]
+    name: 'RPM',
+    data: []
   }],
-  labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
   yaxis: {
     labels: {
       formatter: function(value, index) {
@@ -126,107 +72,128 @@ var d_2options2 = {
     },
     theme: 'dark'
   },
-  colors: ['#fff']
+  colors: ['#fff'],
+  noData: {
+    text: "No Data",
+    align: 'center',
+    verticalAlign: 'middle',
+    offsetX: 0,
+    offsetY: 0,
+    style: {
+      color: undefined,
+      fontSize: '14px',
+      fontFamily: undefined
+    }
+  }
+}
+
+/*
+    =============================
+        Charge | Options
+    =============================
+*/
+var d_2options2 = {
+  chart: {
+    id: 'sparkline1',
+    group: 'sparklines',
+    type: 'area',
+    height: 280,
+    sparkline: {
+      enabled: true
+    },
+  },
+  stroke: {
+      curve: 'smooth',
+      width: 2
+  },
+  fill: {
+    opacity: 1,
+  },
+  series: [{
+    name: 'Charge %',
+    data: []
+  }],
+  yaxis: {
+    labels: {
+      formatter: function(value, index) {
+        return (value)
+      },
+      offsetX: -22,
+      offsetY: 0,
+      style: {
+          fontSize: '12px',
+          fontFamily: 'Nunito, sans-serif',
+          cssClass: 'apexcharts-yaxis-title',
+      },
+    }
+  },
+  grid: {
+    padding: {
+      top: 125,
+      right: 0,
+      bottom: 36,
+      left: 0
+    }, 
+  },
+  fill: {
+      type:"gradient",
+      gradient: {
+          type: "vertical",
+          shadeIntensity: 1,
+          inverseColors: !1,
+          opacityFrom: .40,
+          opacityTo: .05,
+          stops: [45, 100]
+      }
+  },
+  tooltip: {
+    enabled: true,
+    x: {
+      show: true,
+    },
+    theme: 'dark'
+  },
+  colors: ['#fff'],
+  noData: {
+    text: "No Data",
+    align: 'center',
+    verticalAlign: 'middle',
+    offsetX: 0,
+    offsetY: 0,
+    style: {
+      color: undefined,
+      fontSize: '14px',
+      fontFamily: undefined
+    }
+  }
 }
 
 /*
     =================================
-        Revenue Monthly | Options
+      GG Plot | Options
     =================================
 */
 var options1 = {
   chart: {
     fontFamily: 'Nunito, sans-serif',
     height: 365,
-    type: 'area',
+    type: 'scatter',
     zoom: {
-        enabled: false
-    },
-    dropShadow: {
-      enabled: true,
-      opacity: 0.3,
-      blur: 5,
-      left: -7,
-      top: 22
+        enabled: true
     },
     toolbar: {
-      show: false
+      show: true
     },
-    events: {
-      mounted: function(ctx, config) {
-        const highest1 = ctx.getHighestValueInSeries(0);
-        const highest2 = ctx.getHighestValueInSeries(1);
-
-        ctx.addPointAnnotation({
-          x: new Date(ctx.w.globals.seriesX[0][ctx.w.globals.series[0].indexOf(highest1)]).getTime(),
-          y: highest1,
-          label: {
-            style: {
-              cssClass: 'd-none'
-            }
-          },
-          customSVG: {
-              SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#1b55e2" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
-              cssClass: undefined,
-              offsetX: -8,
-              offsetY: 5
-          }
-        })
-
-        ctx.addPointAnnotation({
-          x: new Date(ctx.w.globals.seriesX[1][ctx.w.globals.series[1].indexOf(highest2)]).getTime(),
-          y: highest2,
-          label: {
-            style: {
-              cssClass: 'd-none'
-            }
-          },
-          customSVG: {
-              SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#e7515a" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
-              cssClass: undefined,
-              offsetX: -8,
-              offsetY: 5
-          }
-        })
-      },
-    }
   },
-  colors: ['#1b55e2', '#e7515a'],
+  colors: ['#1b55e2' ],
   dataLabels: {
       enabled: false
-  },
-  markers: {
-    discrete: [{
-    seriesIndex: 0,
-    dataPointIndex: 7,
-    fillColor: '#000',
-    strokeColor: '#000',
-    size: 5
-  }, {
-    seriesIndex: 2,
-    dataPointIndex: 11,
-    fillColor: '#000',
-    strokeColor: '#000',
-    size: 4
-  }]
-  },
-  subtitle: {
-    text: 'm/s²',
-    align: 'left',
-    margin: 0,
-    offsetX: -10,
-    offsetY: 35,
-    floating: false,
-    style: {
-      fontSize: '14px',
-      color:  '#bfc9d4'
-    }
   },
   title: {  
     text: '⠀',
     align: 'left',
     margin: 0,
-    offsetX: -10,
+    offsetX: 0,
     offsetY: 0,
     floating: false,
     style: {
@@ -237,17 +204,13 @@ var options1 = {
   stroke: {
       show: true,
       curve: 'smooth',
-      width: 2,
-      lineCap: 'square'
+      width: 0.1,
+      lineCap: 'round'
   },
   series: [{
-      name: 'AccelX',
-      data: [16800, 16800, 15500, 17800, 15500, 17000, 19000, 16000, 15000, 17000, 14000, 17000]
-  }, {
       name: 'AccelY',
-      data: [16500, 17500, 16200, 17300, 16000, 19500, 16000, 17000, 16000, 19000, 18000, 19000]
+      data: [[-16800, 16500], [16800, -17500], [-15500, -16200], [0, 0], [100, 300], [1500, 1700], [1700, 1600], [1900, 1950], [1600, 1000], [-1500, 1700], [1700, -1000], [-1400, -1800], [17000, 19000]]
   }],
-  labels: [],
   xaxis: {
     axisBorder: {
       show: false
@@ -260,11 +223,14 @@ var options1 = {
     },
     labels: {
       offsetX: 0,
-      offsetY: 5,
+      offsetY: 0,
       style: {
           fontSize: '12px',
           fontFamily: 'Nunito, sans-serif',
           cssClass: 'apexcharts-xaxis-title',
+          formatter: function(val) {
+            return parseFloat(val).toFixed(1)
+          }
       },
     }
   },
@@ -284,7 +250,7 @@ var options1 = {
   },
   grid: {
     borderColor: '#191e3a',
-    strokeDashArray: 5,
+    strokeDashArray: 0.9,
     xaxis: {
         lines: {
             show: true
@@ -292,67 +258,68 @@ var options1 = {
     },   
     yaxis: {
         lines: {
-            show: false,
+            show: true,
         }
     },
-    padding: {
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: -10
-    }, 
-  }, 
-  legend: {
-    position: 'top',
-    horizontalAlign: 'right',
-    offsetY: -50,
-    fontSize: '16px',
-    fontFamily: 'Nunito, sans-serif',
-    markers: {
-      width: 10,
-      height: 10,
-      strokeWidth: 0,
-      strokeColor: '#fff',
-      fillColors: undefined,
-      radius: 12,
-      onClick: undefined,
-      offsetX: 0,
-      offsetY: 0
-    },    
-    itemMargin: {
-      horizontal: 0,
-      vertical: 20
-    }
   },
   tooltip: {
-    enabled: false,
+    enabled: true,
     theme: 'dark',
     marker: {
       show: true,
     },
     x: {
-      show: false,
+      show: true,
+    },
+    y: {
+      show: true,
     }
   },
   fill: {
-      type:"gradient",
-      gradient: {
-          type: "vertical",
-          shadeIntensity: 1,
-          inverseColors: !1,
-          opacityFrom: .28,
-          opacityTo: .05,
-          stops: [45, 100]
-      }
-  },
-  responsive: [{
-    breakpoint: 575,
-    options: {
-      legend: {
-          offsetY: -30,
-      },
+    type: 'solid',
+    pattern: {
+      style: 'circles',
+      strokeWidth: 0.1,
     },
-  }]
+  },
+  annotations: {
+    position: 'front' ,
+    yaxis: [{
+        y: 0,
+        y2: null,
+        strokeDashArray: 1,
+        borderColor: '#c2c2c2',
+        fillColor: '#c2c2c2',
+        opacity: 0.3,
+        offsetX: 0,
+        offsetY: 0,
+        yAxisIndex: 0,
+    }],
+    xaxis: [{
+      x: 0,
+      x2: null,
+      strokeDashArray: 1,
+      borderColor: '#c2c2c2',
+      fillColor: '#c2c2c2',
+      opacity: 0.3,
+      offsetX: 0,
+      offsetY: 0,
+    }],
+  },
+  markers: {
+    size: 1,
+    colors: '#1b55e2',
+    strokeColors: '#1b55e2',
+    strokeWidth: 1,
+    strokeOpacity: 1.0,
+    strokeDashArray: 0,
+    fillOpacity: 1,
+    discrete: [],
+    shape: "circle",
+    radius: 1,
+    offsetX: 0,
+    offsetY: 0,
+}
 }
 
 /*
@@ -369,6 +336,24 @@ var sline = {
     },
     toolbar: {
       show: false,
+    },
+  },tooltip: {
+    enabled: false,
+  },xaxis: {
+    labels: {
+      show: false
+    },
+    axisBorder: {
+      show: false,
+    },
+    // type: 'numeric'
+  },
+  yaxis: {
+    labels: {
+      show: false
+    },
+    axisBorder: {
+      show: false,
     }
   },
   dataLabels: {
@@ -379,7 +364,7 @@ var sline = {
   },
   series: [{
     name: "Coordinates",
-    data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    data: []
   }],
   title: {
     show: false,
@@ -387,16 +372,19 @@ var sline = {
   grid: {
     show: false,
   },
-  xaxis: {
-    labels: {
-      show: false
-    },
-    axisBorder: {
-      show: false,
+  noData: {
+    text: "No Data",
+    align: 'center',
+    verticalAlign: 'middle',
+    offsetX: 0,
+    offsetY: 0,
+    style: {
+      color: undefined,
+      fontSize: '14px',
+      fontFamily: undefined
     }
   }
 }
-
 
 /*
     ==============================
